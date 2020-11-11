2a9d443d23a802cbdf57a1feae5221dd5f0c9f72
Jason Wilson <jason.wilson2@marist.edu>
2020-11-11 14:32:38 -0500

    Changes

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ app.post('/single', upload_dest.single('profile'), (req, res) => {
   }
 });
 
+// app.put('/single', upload_dest.single('avatar'), (req, res) => {
+//   try {
+//     res.send(req.file);
+//     console.log('A File Was Uploaded')
+//     console.log(req.file)
+//   }catch(err) {
+//     res.send(400);
+//   }
+// });
+
+app.post('/array', upload_dest.array('profile', 3), (req, res) => {
+  try {
+    res.send(req.files);
+    console.log('Files Have Been Uploaded')
+    console.log(req.files);
+  }catch(err) {
+    res.send(400);
+  }
+});
+
 app.listen(port, () => {
     console.log('listening to the port: ' + port);
 });
